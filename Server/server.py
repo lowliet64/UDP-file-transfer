faba0e168e5911f16ad20845c0f80c66382ca461
GABRIEL HOLANDA CARDOSO <g.holanda@academico.ifrn.edu.br>
2020-10-18 10:52:25 -0300

    adicionando servidor

---
+++ Server/server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+import time
+import udp_server
+from datetime import datetime
+
+class serverUDP(udp_server.UDPServer):
+    def __init__(self,host,port):
+        super().__init__(host,port)
+        self.socket_lock = threading.Lock()
+
+    def handle_requests(self, data, endereco):
+        posicao = data
+
+        self.printwt(f'[ O cliente {endereco} está na posição {posicao}]')
+
+        resposta = f"você está na posição {posicao}"
+        with self.socket_lock:
+            self.sock.sendto(resposta.encode('utf-8'), endereco)
+
+    def receberClientes(self):
+        try:
+            while True:
+                try:
+                    cliente, endereco = self.sock.recvfrom(1024)
+                    c_thread = threading.Thread(target= self.handle_requests, args=(data,endereco))
+                    c_thread.daemon = True
+                    c_thread.start()
+                except OSError as err:
+                    self.printwt(err)
+        except KeyboardInterrupt:
+            self.shutdown_server()
+
+def main():
+    IPlocal = "127.0.0.1"
+    Porta = 8081
+
+    serverGPS = serverUDP(IPlocal, Porta)
+    serverGPS.configure_server()
+    serverGPS.receberClientes()
+
+if __name__ == '__main__':
+    main()
